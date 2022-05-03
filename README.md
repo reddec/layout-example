083b707521ac2b2a29e32ff634a3cd8fb0a97c86
Aleksandr Baryshnikov <owner@reddec.net>
2022-05-03 15:26:55 +0800

    add content

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # layout-example
-Example repo for layout
+
+Example repo for layout wich demonstrates almost all features.
---
+++ hooks/info.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This is just for example.
+# In normal scenario - use README as template
+
+echo "# {{.project_name}}" > README.md
+date >> README.md
